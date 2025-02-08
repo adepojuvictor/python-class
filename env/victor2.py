07037bcc5942418a2f6985eb3014bc90d8c37d66
Adepoju victor <adepojuvictor2705@gmail.com>
2025-02-08 10:07:21 +0100

    i did my first project

---
+++ env/victor2.py
@@ -0,0 +1,14 @@
+#st = open ("st.txt", "w") 
+#st.write("hello,world")
+#st.close()
+#with open("student_data.txt", "w") as f :
+    #f.write("victor is a big boy, living a big boy life")
+   # print(f.write())
+
+
+st = open ("st.txt", "w")
+st.write("victor is a programmer")
+st.close()
+with open("my,story,my autobiography.txt", "w") as g :
+    g.write("victor is a son of God")
+    print(g.write())
